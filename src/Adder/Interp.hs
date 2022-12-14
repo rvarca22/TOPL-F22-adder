183976bce4590bc33b71b59b6d0ec201514e1f3d
Stubeans <stuart.smith2@marist.edu>
2022-12-14 17:38:55 -0500

    fix # 1

--- src/Adder/Interp.hs
+++ src/Adder/Interp.hs
@@ -20,7 +20,7 @@ import Adder.DataStructures (DenVal, Environment, ExpVal (..), Function (..))
 import Adder.Defs (Source)
 import Adder.Environment (Env (..))
 import Adder.Lang.Parser (ParseError, parseFile, parseInteractive)
-import Adder.Lang.Syntax (Expression (..), Program (..), Statement (..))
+import Adder.Lang.Syntax (Expression (..), Program (..), Statement (..), BinaryOp (..))
 import Adder.Store (Store, deref, emptyStore, newref, setref)
 import Data.Either (fromRight)
 import Prelude hiding (exp)
@@ -49,7 +49,20 @@ resultOfProgram _ env st0 = undefined
 -- TODO Implement the semantics for each kind of Adder statement
 
 resultOf :: Statement -> Environment -> Store -> IO Store
+resultOf (StmtList []) env st0 = return st0
+resultOf (StmtList (stmt : stmts)) env st0 = do
+  st1 <- resultOf stmt env st0
+  resultOf (StmtList stmts) env st1
+resultOf (PassStmt) env st = return st
 resultOf _ env st0 = undefined
+
+{-
+resultOf (IfStmt test conseq) p st = if q then st2 else st3
+  where
+    Answer (BoolVal q) st1 = valueOf test p st
+    st2 = resultOf conseq p st1
+    st3 = resultOf conseq p st2
+-}
 
 {- Evaluating a program yields an "answer" - a value and a resulting state. -}
 type Answer = (ExpVal, Store)
@@ -57,9 +70,34 @@ type Answer = (ExpVal, Store)
 {- semantic reductions for expressions -}
 
 -- TODO Implement the semantics for each kind of Adder expression
+
 valueOf :: Expression -> Environment -> Store -> Answer
 valueOf _ env st0 = undefined
+
+-- Binary Operation
+-- Don't forget about free store
+
+valueOf (BinaryExpr op exp1 exp2) env st0 = valueOfBop op val1 val2
+  where
+    (val1, st1) = valueOf exp1  env st0
+    (val2, st2) = valueOf exp2 env st1
+
+
+--valueOF :: assignmentExpr ->  ??
 
 {- Auxiliary functions -}
 -- TODO Implement any helper functions needed to simplify the design of the
 -- interpreter (e.g., the applyProcedure helper function).
+
+valueOfBop :: BinaryOp -> ExpVal -> ExpVal -> (ExpVal, Store)
+valueOfBop op val1 val2 = case op of
+  _ -> error "unimplemented binary operation"
+  --Code here
+
+--valueOf :: Return -> Environment -> Store -> Answer
+--valueOf (Return exp1) env store = env2 --Added Exp 1 into the parathenses
+--Answer Return exp1 env = exp1         --Attempted to add the return statement
+
+-- valueOf(Return exp1)env = env1 exp2
+---------------------------------------------
+-- valueOf(exp1)env = env1 exp2
