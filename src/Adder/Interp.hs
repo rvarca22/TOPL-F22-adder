b7e7550dcf7dba56f83d668174ca23d1d43c353a
RichieConsiglio <richard.consiglio1@marist.edu>
2022-12-16 14:36:17 -0500

    Interp Support for BinaryOps add, concat, subtract, mult, floor divide, and power

--- src/Adder/Interp.hs
+++ src/Adder/Interp.hs
@@ -19,11 +19,13 @@ import Adder.DataStructures (DenVal, Environment, ExpVal (..), Function (..))
 -- import Adder.TypeEnv (TEnv (..), TypeEnvironment)
 import Adder.Defs (Source)
 import Adder.Environment (Env (..))
-import Adder.Lang.Parser (ParseError, parseFile, parseInteractive)
-import Adder.Lang.Syntax (Expression (..), Program (..), Statement (..))
+import Adder.Lang.Parser (ParseError, parseFile, parseInteractive,)
+import Adder.Lang.Syntax (Expression (..), Program (..), Statement (..), ExpVal (..))
+
 import Adder.Store (Store, deref, emptyStore, newref, setref)
 import Data.Either (fromRight)
 import Prelude hiding (exp)
+import Adder.Lang.Syntax(BinaryOp (Plus, Minus, Times, FloorDiv, Power))
 
 type Interpreter a = a -> Environment -> Store -> IO Store
 
@@ -63,3 +65,36 @@ valueOf _ env st0 = undefined
 {- Auxiliary functions -}
 -- TODO Implement any helper functions needed to simplify the design of the
 -- interpreter (e.g., the applyProcedure helper function).
+valueOfBop :: BinaryOp -> ExpVal -> ExpVal -> ExpVal
+valueOfBop op val1 val2 = case op of
+  Plus -> case (val1, val2) of
+    (IntVal n1, IntVal n2) -> IntVal(n1 'plus` n2)
+    (IntVal n1, FloatVal n2) -> FloatVal(n1 `plus` n2)
+    (FloatVal n1, IntVal n2) -> FloatVal(n1 `plus` n2)
+    (FloatVal n1, FloatVal n2) -> FloatVal(n1 `plus` n2)
+    (StrVal n1, StrVal n2) -> StrVal(n1 `plus` n2)
+    _ -> undefined
+  Minus -> case (val1, val2) of
+    (IntVal n1, IntVal n2) -> IntVal(n1 `minus` n2)
+    (IntVal n1, FloatVal n2) -> FloatVal(n1 `minus` n2)
+    (FloatVal n1, IntVal n2) -> FloatVal(n1 `minus` n2)
+    (FloatVal n1, FloatVal n2) -> FloatVal(n1 `minus` n2)
+    _ -> undefined
+  Times -> case (val1, val2) of
+    (IntVal n1, IntVal n2) -> IntVal(n1 `times` n2)
+    (IntVal n1, FloatVal n2) -> FloatVal(n1 `times` n2)
+    (FloatVal n1, IntVal n2) -> FloatVal(n1 `times` n2)
+    (FloatVal n1, FloatVal n2) -> FloatVal(n1 `times` n2)
+    _ -> undefined
+  FloorDiv -> case (val1, val2) of
+    (IntVal n1, IntVal n2) -> IntVal(n1 `floordiv` n2)
+    (IntVal n1, FloatVal n2) -> FloatVal(n1 `floordiv` n2)
+    (FloatVal n1, IntVal n2) -> FloatVal(n1 `floordiv` n2)
+    (FloatVal n1, FloatVal n2) -> FloatVal(n1 `floordiv` n2)
+    _ -> undefined
+  Power -> case (val1, val2) of
+    (IntVal n1, IntVal n2) -> IntVal(n1 `power` n2)
+    (IntVal n1, FloatVal n2) -> FloatVal(n1 `power` n2)
+    (FloatVal n1, IntVal n2) -> FloatVal(n1 `power` n2)
+    (FloatVal n1, FloatVal n2) -> FloatVal(n1 `power` n2)
+    _ -> undefined
